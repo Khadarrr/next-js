8e781145cbdc0da0068089dfcbb4345664f77629
Khadarrr <111360349+Khadarrr@users.noreply.github.com>
2023-09-06 11:47:46 +0200

    test

--- app/posts/route.js
+++ app/posts/route.js
@@ -3,3 +3,12 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
     return NextResponse.json({posts: "successsss"}, {status: 300});
 }
+
+const data = {
+    message: 'Hello, this is your custom API!',
+    timestamp: new Date().toISOString(),
+  };
+  
+  export default function handler(req, res) {
+    res.status(200).json(data);
+  }
